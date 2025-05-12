a4bbd4268ce4934d72c4b98884c71cfe10e1254e
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-05-12 22:49:55 +0530

    Time: 327 ms (23.3%), Space: 14.1 MB (39.08%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -5,30 +5,35 @@ public:
         for (int i = 0; i <= digits.size() - 3; i++) {
             for (int j = i + 1; j <= digits.size() - 2; j++) {
                 for (int k = j + 1; k <= digits.size() - 1; k++) {
-                    int n1 = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    int n2 = digits[i] * 100 + digits[k] * 10 + digits[j];
-                    int n3 = digits[j] * 100 + digits[i] * 10 + digits[k];
-                    int n4 = digits[j] * 100 + digits[k] * 10 + digits[i];
-                    int n5 = digits[k] * 100 + digits[i] * 10 + digits[j];
-                    int n6 = digits[k] * 100 + digits[j] * 10 + digits[i];
-                    if (n1 >= 100&&n1%2==0)
+                    if (digits[i] > 0 && digits[k] % 2 == 0) {
+                        int n1 = digits[i] * 100 + digits[j] * 10 + digits[k];
                         temp.insert(n1);
-                    if (n2 >= 100&&n2%2==0)
+                    }
+                    if (digits[i] > 0 && digits[j] % 2 == 0) {
+                        int n2 = digits[i] * 100 + digits[k] * 10 + digits[j];
                         temp.insert(n2);
-                    if (n3 >= 100&&n3%2==0)
+                    }
+                    if (digits[j] > 0 && digits[k] % 2 == 0) {
+                        int n3 = digits[j] * 100 + digits[i] * 10 + digits[k];
                         temp.insert(n3);
-                    if (n4 >= 100&&n4%2==0)
+                    }
+                    if (digits[j] > 0 && digits[i] % 2 == 0) {
+                        int n4 = digits[j] * 100 + digits[k] * 10 + digits[i];
                         temp.insert(n4);
-                    if (n5 >= 100&&n5%2==0)
+                    }
+                    if (digits[k] > 0 && digits[j] % 2 == 0) {
+                        int n5 = digits[k] * 100 + digits[i] * 10 + digits[j];
                         temp.insert(n5);
-                    if (n6 >= 100&&n6%2==0)
+                    }
+                    if (digits[k] > 0 && digits[i] % 2 == 0) {
+                        int n6 = digits[k] * 100 + digits[j] * 10 + digits[i];
                         temp.insert(n6);
+                    }
                 }
             }
         }
-        vector<int>ans;
-        for(auto i: temp)
-        {
+        vector<int> ans;
+        for (auto i : temp) {
             ans.push_back(i);
         }
         return ans;
