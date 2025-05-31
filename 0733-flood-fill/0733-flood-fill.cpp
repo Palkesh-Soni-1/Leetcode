091c35bce8a78f0b429ad007cde1b4126fce9068
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-05-31 11:03:07 +0530

    Time: 0 ms (100%), Space: 18 MB (89.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,24 +5,33 @@ class Solution {
         // int n = temp.size();
         // int m = temp[0].size();
         temp[sr][sc]=color;
-        if((sr-1)>=0&&image[sr-1][sc]==imp&&temp[sr-1][sc]!=color)
+
+        int d[] = {-1,0,1,0};
+        int f[] = {0,-1,0,1};
+
+        for(int i=0;i<4;i++)
         {
-            dfs(sr-1,sc,imp,color,temp,image,n,m);
+            if(sr+d[i]>=0&&sr+d[i]<n&&sc+f[i]>=0&&sc+f[i]<m&&image[sr+d[i]][sc+f[i]]==imp&&temp[sr+d[i]][sc+f[i]]!=color)
+            dfs(sr+d[i],sc+f[i],imp,color,temp,image,n,m);
         }
-        if((sr+1)<n&&(image[sr+1][sc]==imp)&&temp[sr+1][sc]!=color)
-        {
-            // cout<<"run"<<endl;
-            // cout<<sr<<" "<<sc<<endl;
-            dfs(sr+1,sc,imp,color,temp,image,n,m);
-        }
-        if(sc-1>=0&&image[sr][sc-1]==imp&&temp[sr][sc-1]!=color)
-        {
-            dfs(sr,sc-1,imp,color,temp,image,n,m);
-        }
-        if(sc+1<m&&image[sr][sc+1]==imp&&temp[sr][sc+1]!=color)
-        {
-            dfs(sr,sc+1,imp,color,temp,image,n,m);
-        }
+        // if((sr-1)>=0&&image[sr-1][sc]==imp&&temp[sr-1][sc]!=color)
+        // {
+        //     dfs(sr-1,sc,imp,color,temp,image,n,m);
+        // }
+        // if((sr+1)<n&&(image[sr+1][sc]==imp)&&temp[sr+1][sc]!=color)
+        // {
+        //     // cout<<"run"<<endl;
+        //     // cout<<sr<<" "<<sc<<endl;
+        //     dfs(sr+1,sc,imp,color,temp,image,n,m);`
+        // }
+        // if(sc-1>=0&&image[sr][sc-1]==imp&&temp[sr][sc-1]!=color)
+        // {
+        //     dfs(sr,sc-1,imp,color,temp,image,n,m);
+        // }
+        // if(sc+1<m&&image[sr][sc+1]==imp&&temp[sr][sc+1]!=color)
+        // {
+        //     dfs(sr,sc+1,imp,color,temp,image,n,m);
+        // }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
@@ -30,13 +39,7 @@ public:
         int colo = color;
         int n = temp.size();
         int m = temp[0].size();
-        // int n = temp.size();
-        // int m = temp[0].size();
-        // sr = 1;
-        // sc = 0;
         int imp = temp[sr][sc];
-        // cout<<imp<<endl;
-        // cout<<n<<" "<<m<<endl;
         dfs(sr,sc,imp,colo,temp,image,n,m);
         return temp;
     }
