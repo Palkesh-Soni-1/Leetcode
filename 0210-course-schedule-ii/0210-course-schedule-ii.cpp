49680655f289a2dfb5652b76433aaa2f962d857c
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-03 14:12:12 +0530

    Time: 4 ms (57.93%), Space: 18.6 MB (49.1%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,73 +1,52 @@
 class Solution {
-    private:
-    bool cycle(int start, vector<int>&vis,vector<vector<int>>&dum, vector<int>&ans, vector<int>&path)
-    {
-        vis[start]=1;
-        path[start]=1;
-        for(auto i:dum[start])
+    private: 
+    vector<int> bfs(int numCourses, vector<vector<int>>& prerequisites,int n,  vector<vector<int>>adj){
+
+        //topological sort -> kahn's algorithm => detect cycle in directed graph using bfs
+        vector<int>ans;
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<n;i++)
         {
-            if(vis[i]==0)
+            indegree[prerequisites[i][1]]++;
+        }
+        queue<int>q;
+        int count=0;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
             {
-                if(cycle(i,vis,dum,ans,path)==false)
+                q.push(i);
+                count++;
+            }
+        }
+        while(!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+            for(int i:adj[x])
+            {
+                indegree[i]--;
+                if(indegree[i]==0)
                 {
-                 return false;
+                    count++;
+                    q.push(i);
                 }
             }
-            else if(path[i]==1)
-            {
-                path[i]=0;
-                return false;
-            }
         }
-        path[start]=0;
-        return true;
-    }
-    void dfs(int start,vector<int>&visi,vector<vector<int>>&dum, vector<int>&ans)
-    {
-        visi[start]=1;
-        for(auto i:dum[start])
-        {
-            if(visi[i]==0)
-            {
-                dfs(i,visi,dum,ans);
-            }
-        }
-        ans.push_back(start);
+        reverse(ans.begin(), ans.end());
+        if(count>=numCourses)
+        return ans;
+        return {};
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n = prerequisites.size();
-        vector<int>ans;
-        vector<int>vis(numCourses,0);
-        vector<int>visi(numCourses,0);
-        vector<int>path(numCourses,0);
-        vector<vector<int>>dum(numCourses);
-        for(int i=0;i<numCourses;i++)
+        vector<vector<int>>adj(numCourses);
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(prerequisites[j][0]==i)
-                {
-                    dum[i].push_back(prerequisites[j][1]);
-                }
-            }
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        for(int i=0;i<numCourses;i++)
-        {
-            if(vis[i]==0)
-            {
-                if(!cycle(i,vis,dum,ans,path))
-                return ans;
-            }
-        }
-        // cout<<"Reach"<<endl;
-        for(int i=0;i<numCourses;i++)
-        {
-            if(visi[i]==0)
-            {
-                dfs(i,visi,dum,ans);
-            }
-        }
-        return ans;
+        return bfs(numCourses, prerequisites, n, adj);
     }
 };
