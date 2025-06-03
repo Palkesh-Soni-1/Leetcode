3c97ca010ca01d36624ece825374056b5fcce657
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-03 14:00:18 +0530

    Time: 3 ms (80.24%), Space: 20.2 MB (25.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,75 +1,105 @@
 class Solution {
-    private:
-    bool cycle(int start, vector<int>&vis,vector<vector<int>>&dum, vector<int>&ans, vector<int>&path)
+private:
+    bool dfs(int start, vector<vector<int>>adj, vector<int>&vis, vector<int>&path)
     {
+
+        //===tle
         vis[start]=1;
         path[start]=1;
-        for(auto i:dum[start])
+        for(auto i: adj[start])
         {
             if(vis[i]==0)
             {
-                if(cycle(i,vis,dum,ans,path)==false)
-                {
-                 return false;
-                }
+                if(dfs(i, adj, vis, path)==false)
+                return false;
             }
             else if(path[i]==1)
-            {
-                path[i]=0;
+            return false;
+            else{
+                if(dfs(i, adj, vis, path)==false)
                 return false;
             }
         }
         path[start]=0;
         return true;
     }
-    void dfs(int start,vector<int>&visi,vector<vector<int>>&dum, vector<int>&ans)
-    {
-        visi[start]=1;
-        for(auto i:dum[start])
+
+    bool bfs(int numCourses, vector<vector<int>>& prerequisites,int n,  vector<vector<int>>adj){
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<n;i++)
         {
-            if(visi[i]==0)
+            indegree[prerequisites[i][1]]++;
+        }
+        queue<int>q;
+        int count=0;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
             {
-                dfs(i,visi,dum,ans);
+                q.push(i);
+                count++;
             }
         }
-        ans.push_back(start);
+        while(!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            for(int i:adj[x])
+            {
+                indegree[i]--;
+                if(indegree[i]==0)
+                {
+                    count++;
+                    q.push(i);
+                }
+            }
+        }
+        if(count>=numCourses)
+        return true;
+        return false;
+
+
+
+
+        //======wrong approach
+        // queue<int>q;
+        // q.push(start);
+        // vis[start]=1;
+        // while(!q.empty())
+        // {
+        //     auto x = q.front();
+        //     q.pop();
+        //     for(auto i: adj[x])
+        //     {
+        //         if(vis[i]==1)
+        //         return false;
+        //         else{
+        //             vis[i]=1;
+        //             q.push(i);
+        //         }
+        //     }
+        // }
+        // return true;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = prerequisites.size();
-        vector<int>ans;
+        vector<vector<int>>adj(numCourses);
+        for(int i=0;i<n;i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
         vector<int>vis(numCourses,0);
-        vector<int>visi(numCourses,0);
         vector<int>path(numCourses,0);
-        vector<vector<int>>dum(numCourses);
-        for(int i=0;i<numCourses;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(prerequisites[j][0]==i)
-                {
-                    dum[i].push_back(prerequisites[j][1]);
-                }
-            }
-        }
-        for(int i=0;i<numCourses;i++)
-        {
-            if(vis[i]==0)
-            {
-                if(!cycle(i,vis,dum,ans,path))
-                return false;
-            }
-        }
-        // cout<<"Reach"<<endl;
-        for(int i=0;i<numCourses;i++)
-        {
-            if(visi[i]==0)
-            {
-                dfs(i,visi,dum,ans);
-            }
-        }
-        if(ans.size()==numCourses)
-        return true;
-        return false;
+        // for(int i=0;i<numCourses;i++)
+        // {
+        //     if(vis[i]==0)
+        //     {
+        //         // if(dfs(i, adj, vis, path)==false)
+        //         // return false;
+        //     }
+        // }
+        // return true;
+        return bfs(numCourses, prerequisites, n, adj);
     }
 };
