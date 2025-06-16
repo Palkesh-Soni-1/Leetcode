a7fc3c84fb5b5ad404ae392dbe243decf557e961
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-16 21:49:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (6.27%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
+        stack<pair<int, int>> st; // {value, index}
         int diff = -1;
-        int n = nums.size();
-        
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[j] > nums[i]){
-                    diff = max(diff, nums[j] - nums[i]);
-                }
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Maintain monotonic stack of mins
+            if (st.empty() || nums[i] < st.top().first) {
+                st.push({nums[i], i});
+            }
+
+            // Always compare with the bottom-most (minimum) element
+            if (!st.empty() && st.top().second < i && nums[i] > st.top().first) {
+                diff = max(diff, nums[i] - st.top().first);
             }
         }
 
