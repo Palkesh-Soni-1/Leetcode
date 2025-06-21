8a690f962c11a616b4c4071effcad67fc4d3c025
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-21 20:16:40 +0530

    Time: 14 ms (87.93%), Space: 30.9 MB (96.98%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -2,25 +2,50 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>>ans(2);
-        map<int,int>m1,m2;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i=0,j=0;
         int n1 = nums1.size();
         int n2 = nums2.size();
-        for(int i=0;i<n1;i++)
+        while(i<n1&&j<n2)
         {
-            m1[nums1[i]]++;
-        }
-        for(int i=0;i<n2;i++){
-            m2[nums2[i]]++;
-        }
-        for(auto i:m1){
-            if(m2.find(i.first)==m2.end()){
-                ans[0].push_back(i.first);
+            if(nums1[i]==nums2[j])
+            {
+                i++;
+                j++;
+                while(i<n1&&nums1[i-1]==nums1[i])
+                i++;
+                while(j<n2&&nums2[j-1]==nums2[j])
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                ans[0].push_back(nums1[i]);
+                i++;
+                while(i<n1&&nums1[i-1]==nums1[i])
+                i++;
+            }
+            else
+            {
+                ans[1].push_back(nums2[j]);
+                j++;
+                while(j<n2&&nums2[j-1]==nums2[j])
+                j++;
             }
         }
-        for(auto i:m2){
-            if(m1.find(i.first)==m1.end()){
-                ans[1].push_back(i.first);
-            }
+        while(i<n1)
+        {
+            ans[0].push_back(nums1[i]);
+            i++;
+            while(i<n1&&nums1[i-1]==nums1[i])
+            i++;
+        }
+        while(j<n2)
+        {
+            ans[1].push_back(nums2[j]);
+            j++;
+            while(j<n2&&nums2[j-1]==nums2[j])
+            j++;
         }
         return ans;
     }
