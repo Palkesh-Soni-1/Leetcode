62ef7f7d7761b465b576f8ce540c556440452bfa
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-21 20:21:54 +0530

    Time: 4 ms (5.1%), Space: 11.9 MB (77.64%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp1,mp2;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp1[arr[i]]++;
+        }
+        for(auto i:mp1)
+        {
+            mp2[i.second]++;
+            if(mp2[i.second]==2)
+            return false;
+        }
+        return true;
+    }
+};
