ef943dbb45a962b09a715f2acb19afc4792ca960
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-06-29 13:39:56 +0530

    Time: 1140 ms (8.46%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,19 @@
+WITH first_login AS (
+    SELECT player_id, MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+),
+next_day_login AS (
+    SELECT f.player_id
+    FROM first_login f
+    JOIN Activity a 
+      ON f.player_id = a.player_id 
+     AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+)
+
+SELECT 
+    ROUND(COUNT(DISTINCT n.player_id) / COUNT(DISTINCT f.player_id), 2) AS fraction
+FROM 
+    first_login f
+LEFT JOIN 
+    next_day_login n ON f.player_id = n.player_id;
