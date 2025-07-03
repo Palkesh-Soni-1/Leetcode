b3595958ab7211100cd5094c1959d39bdda3b7b3
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-07-03 18:23:04 +0530

    Time: 5 ms (55.43%), Space: 23.9 MB (57.72%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(), intervals.end());
+        int mini = intervals[0][0];
+        int maxi = intervals[0][1];
+        int n = intervals.size();
+        for(int i=1;i<n;i++)
+        {
+            if(intervals[i][0]>=mini&&intervals[i][0]<=maxi)
+            {
+                maxi = max(maxi, intervals[i][1]);
+            }
+            else{
+                ans.push_back({mini, maxi});
+                mini = intervals[i][0];
+                maxi = intervals[i][1];
+            }
+        }
+        ans.push_back({mini, maxi});
+        return ans;
+    }
+};
