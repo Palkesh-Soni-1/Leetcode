572ee2653b2b460af1b797c48834a0e0ea728ffa
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-07-24 00:41:10 +0530

    Time: 19 ms (94.65%), Space: 28.3 MB (53.96%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumGain(std::string s, int x, int y) {
+        if (x < y) {
+            std::swap(x, y);
+            std::reverse(s.begin(), s.end());
+        }
+
+        long long totalPoints = 0;
+
+        std::vector<char> stack1;
+        for (char c : s) {
+            if (c == 'b' && !stack1.empty() && stack1.back() == 'a') {
+                stack1.pop_back();
+                totalPoints += x;
+            } else {
+                stack1.push_back(c);
+            }
+        }
+
+        std::vector<char> stack2;
+        for (char c : stack1) {
+            if (c == 'a' && !stack2.empty() && stack2.back() == 'b') {
+                stack2.pop_back();
+                totalPoints += y;
+            } else {
+                stack2.push_back(c);
+            }
+        }
+
+        return totalPoints;
+    }
+};
