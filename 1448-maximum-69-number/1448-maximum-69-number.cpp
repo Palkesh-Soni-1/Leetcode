da3d8b1ff0a5dc5f2ec19331504df1b58ee09cc2
Palkesh Soni <136756658+Palkesh-Soni-1@users.noreply.github.com>
2025-08-16 12:49:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.18%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string temp = to_string(num);
+        int n = temp.size();
+        for(int i=0;i<n;i++)
+        {
+            if(temp[i]=='6')
+            {temp[i]='9';
+            break;}
+        }
+        return stoi(temp);
+    }
+};
